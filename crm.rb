d0b02e7322f10faab5feac1ca0945c10b8adfd96
Ndelforno <nicolas.delforno@gmail.com>
2018-09-20 17:52:55 -0400

    exercise done

---
+++ crm.rb
@@ -0,0 +1,23 @@
+require_relative 'contact'
+require 'sinatra'
+
+get ("/") do
+  erb :index
+end
+
+get ("/about") do
+  erb :about
+end
+
+get ("/index") do
+  erb :index
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+get ("/contacts") do
+  @contact_collection = Contact.all
+  erb :contacts
+end
