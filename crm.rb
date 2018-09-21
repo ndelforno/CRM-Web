e66817e3ca808f4df645dc765fc4827a3348f990
Ndelforno <nicolas.delforno@gmail.com>
2018-09-21 16:08:44 -0400

    part 3 done

--- crm.rb
+++ crm.rb
@@ -22,3 +22,13 @@ get ("/contacts") do
   @contact_collection = Contact.all
   erb :contacts
 end
+
+get '/contacts/:id' do
+  params[:id]
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
