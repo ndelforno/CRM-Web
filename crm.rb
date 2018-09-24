dc4d10ccf9aa13ed61f6711ca2e07ec1f853a637
Ndelforno <nicolas.delforno@gmail.com>
2018-09-24 17:28:30 -0400

    Part 4 done

--- crm.rb
+++ crm.rb
@@ -1,15 +1,15 @@
 require_relative 'contact'
 require 'sinatra'
 
-get "/" do
-  erb :index
+get '/' do
+  redirect to ("/index")
 end
 
-get "/about" do
+get '/about' do
   erb :about
 end
 
-get "/index" do
+get '/index' do
   @contact_collection = Contact.all
   erb :index
 end
@@ -18,7 +18,7 @@ after do
   ActiveRecord::Base.connection.close
 end
 
-get "/contacts" do
+get '/contacts' do
   @contact_collection = Contact.all
   erb :contacts
 end
@@ -33,7 +33,7 @@ get '/contacts/:id' do
   end
 end
 
-get "/new" do
+get '/new' do
   erb :new_contact
 end
 
@@ -53,6 +53,43 @@ get '/contacts/:id/edit' do
   if @contact
     erb :edit_contact
   else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/index/search/:first_name' do
+  p params[:first_name]
+  @contact = Contact.find_by(first_name: params[:first_name])
+  if @contact
+    erb :edit_contact
+  else
+    puts "Contact does not exist"
     raise Sinatra::NotFound
   end
 end
